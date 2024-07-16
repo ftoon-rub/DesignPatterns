cecd55982aeeb4f0c440b65874c962e35cedc40c
Ftoon Alrubayea <ftoon-rubayea@hotmail.com>
2024-07-16 07:38:07 +0300

    create Originator class

---
+++ DesignPatterns/MementoPattern/Originator.cs
@@ -0,0 +1,31 @@
+﻿namespace DesignPatterns.MementoPattern
+{
+    /// <summary>
+    /// creates a Memento containing a snapshot of its current internal state.
+    /// it's controle the undo mechanism, by creating the currnet state and restore the state 
+    /// </summary>
+    internal class Originator
+    {
+        private string Content;
+        public Originator(string Content)
+        {
+            this.Content = Content;
+        }
+        /// <summary>
+        /// create new state (memento) with the currnet content
+        /// </summary>
+        /// <returns></returns>
+        public Memento CreateState()
+        {
+            return new Memento(this.Content);
+        }
+        /// <summary>
+        /// restore the currnet memento state
+        /// </summary>
+        /// <param name="memento"></param>
+        public void Restore(Memento memento)
+        {
+            Content = memento.GetState();
+        }
+    }
+}
