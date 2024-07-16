28e7624fa00ea1cd8b9f8142007cb01371fbba34
Ftoon Alrubayea <ftoon-rubayea@hotmail.com>
2024-07-16 07:13:36 +0300

    create the Memento

---
+++ DesignPatterns/MementoPattern/Memento.cs
@@ -0,0 +1,21 @@
+﻿namespace DesignPatterns.MementoPattern
+{
+    /// <summary>
+    /// it has the content 
+    /// </summary>
+    internal class Memento
+    {
+        private readonly string Content;
+
+        public Memento(string Content)
+        {
+            this.Content = Content;
+        }
+
+        public string GetState()
+        {
+            return this.Content;
+        }
+
+    }
+}
