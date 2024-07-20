8d9043665c582a56addc74d23591fc60ee61bb80
Ftoon Alrubayea <ftoon-rubayea@hotmail.com>
2024-07-20 14:24:20 +0300

    test the abuse of state design pattern

--- DesignPatterns/StatePattern/TestState.cs
+++ DesignPatterns/StatePattern/TestState.cs
@@ -1,14 +1,38 @@
-﻿namespace DesignPatterns.StatePattern
+﻿using DesignPatterns.StatePattern.Exercises;
+using DesignPatterns.StatePattern.Exercises.Abuse;
+using DesignPatterns.StatePattern.Exercises.Ex4;
+
+namespace DesignPatterns.StatePattern
 {
     internal class TestState
     {
         public void Test()
         {
-            Context context = new ();
+            #region ex1
+            Console.WriteLine("------------------Ex1--------------------");
+            Context context = new();
             context.State = new ConcreteStatesA();
             context.Request();
             context.State = new ConcreteStatesB();
             context.Request();
+            #endregion
+
+            #region ex2
+            Console.WriteLine("------------------Ex2 Abuse--------------------");
+            AbuseStopWatch AbuseContext = new AbuseStopWatch();
+            AbuseContext.Click();
+            AbuseContext.Click();
+            AbuseContext.Click ();
+            #endregion
+
+            //#region ex3
+            //Console.WriteLine("------------------Ex3--------------------");
+            //DirectionService directionService = new ();
+            //directionService.directionState = new DrivingState ();
+            //directionService.GetDirection();
+            //directionService.directionState = new WalkingState ();
+            //directionService.GetDirection ();
+            //#endregion
         }
     }
 }
