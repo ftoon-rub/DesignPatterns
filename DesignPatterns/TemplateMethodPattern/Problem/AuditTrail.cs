4b1fb6556f2a2060e22ad8cc3245227ae4421c35
Ftoon Bin Rbia <fbinrbia@bcare.com.sa>
2024-07-23 14:28:33 +0300

    create TemplateMethodPattern folder and files and add the problem that need the DP

---
+++ DesignPatterns/TemplateMethodPattern/Problem/AuditTrail.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPatterns.TemplateMethodPattern
+{
+    internal class AuditTrail
+    {
+        public void record()
+        {
+            Console.WriteLine("audit");
+        }
+    }
+}
