5298697b6473be89f617dd8b6146cecd5d1aed70
Ftoon Alrubayea <ftoon-rubayea@hotmail.com>
2024-07-23 07:28:18 +0300

    edit iterator pattern

--- DesignPatterns/IteratorPattern/ListConcreteAggregate.cs
+++ DesignPatterns/IteratorPattern/ListConcreteAggregate.cs
@@ -2,5 +2,23 @@
 {
     internal class ListConcreteAggregate<T> : IAggregate<T>
     {
+        public T this[int index] => throw new NotImplementedException();
+
+        public int Count => throw new NotImplementedException();
+
+        public IIterator<T> CreateIterator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Pop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Push(T item)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
