209c11887095e21e406bd86b5205550e8ca57204
Ftoon Bin Rbia <fbinrbia@bcare.com.sa>
2024-07-24 13:43:27 +0300

    edit test

--- DesignPatterns/CommandPattern/TestCommandPattern.cs
+++ DesignPatterns/CommandPattern/TestCommandPattern.cs
@@ -1,12 +1,32 @@
-﻿using DesignPatterns.TemplateMethodPattern;
-
-namespace DesignPatterns.CommandPattern
+﻿namespace DesignPatterns.CommandPattern
 {
     internal class TestCommandPattern : ITestState
     {
         public void Test()
         {
             Console.WriteLine("Start TestCommandPattern Class\n");
+
+            Console.WriteLine("What is Behavioral Patterns?");
+            Console.WriteLine("...");
+            Console.WriteLine("\n");
+
+            Console.WriteLine("What is this pattern?");
+            Console.WriteLine("...");
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Pattern Components:");
+            Console.WriteLine("...");
+            Console.WriteLine("...");
+            Console.WriteLine("...");
+            Console.WriteLine("***Look at the popup to see the pattern UML Diagram***");
+            Console.WriteLine("\n");
+
+            Console.WriteLine("SOLID principles used in this pattern");
+            Console.WriteLine("...");
+            Console.WriteLine("...");
+            Console.WriteLine("...");
+            Console.WriteLine("\n");
+
             Console.WriteLine("Problem");
             Console.WriteLine("// 1 problem: \n");
 
@@ -15,6 +35,7 @@ namespace DesignPatterns.CommandPattern
 
             Console.WriteLine("solution");
 
+            Console.WriteLine("Like this");
             Console.WriteLine("\n");
         }
     }
