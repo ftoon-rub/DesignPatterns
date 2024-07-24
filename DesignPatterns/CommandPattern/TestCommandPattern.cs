b3256682d6457ffc8bb720cb7b13751b12454120
Ftoon Bin Rbia <fbinrbia@bcare.com.sa>
2024-07-24 08:30:06 +0300

    create command pattern folders and files

---
+++ DesignPatterns/CommandPattern/TestCommandPattern.cs
@@ -0,0 +1,21 @@
+﻿using DesignPatterns.TemplateMethodPattern;
+
+namespace DesignPatterns.CommandPattern
+{
+    internal class TestCommandPattern : ITestState
+    {
+        public void Test()
+        {
+            Console.WriteLine("Start TestCommandPattern Class\n");
+            Console.WriteLine("Problem");
+            Console.WriteLine("// 1 problem: \n");
+
+            Console.WriteLine("Like this");
+            Console.WriteLine("\n");
+
+            Console.WriteLine("solution");
+
+            Console.WriteLine("\n");
+        }
+    }
+}
