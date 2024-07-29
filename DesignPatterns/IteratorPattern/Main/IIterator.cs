a9013747141fe9ebc3cde2ac0339c7b483e4948e
Ftoon Alrubayea <ftoon-rubayea@hotmail.com>
2024-07-29 06:11:08 +0300

    add commant to define the oop principles and solid princpiles

--- DesignPatterns/IteratorPattern/Main/IIterator.cs
+++ DesignPatterns/IteratorPattern/Main/IIterator.cs
@@ -1,8 +1,16 @@
 ﻿namespace DesignPatterns.IteratorPattern.Main
 {
+    // Interface Segregation Principle (SOLID): IIterator interface is small and focused on a single responsibility.
     public interface IIterator<T>
     {
+        /// <summary>
+        /// Encapsulation (OOP): Exposing only necessary members.
+        /// </summary>
         T Current { get; }
+        /// <summary>
+        /// Single Responsibility Principle (SOLID): Each method does one thing.
+        /// </summary>
+        /// <returns></returns>
         bool HasNext();
         T Next();
         void Reset();
