336901af3a42aee234d9f89f14ae2198629204fc
Ftoon Bin Rbia <fbinrbia@bcare.com.sa>
2024-07-31 10:23:18 +0300

    add observier pattern folders

---
+++ DesignPatterns/ObserverPattern/TestObserverPattern.cs
@@ -0,0 +1,58 @@
+﻿namespace DesignPatterns.ObserverPattern
+{
+    public class TestObserverPattern : ITestState, ITestProblemState, ITestSolutionState
+    {
+        public void Test()
+        {
+            Console.WriteLine("Start TestCommandPattern Class\n");
+
+            Console.WriteLine("What is Behavioral Patterns?");
+            Console.WriteLine("-Deal with how objects interact and communicate. They focus on algorithms and the assignment of responsibilities");
+            Console.WriteLine("\n");
+
+            Console.WriteLine("What is this pattern?");
+            Console.WriteLine("-");
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Pattern Components:");
+            Console.WriteLine("-");
+            Console.WriteLine("-");
+            Console.WriteLine("-");
+            Console.WriteLine("-");
+            Console.WriteLine("-");
+            Console.WriteLine("***Look at the popup to see the pattern UML Diagram***");
+            Console.WriteLine("\n");
+
+            Console.WriteLine("OOP principles used in this pattern");
+            Console.WriteLine("- ");
+            Console.WriteLine("- ");
+            Console.WriteLine("- ");
+            Console.WriteLine("- ");
+            Console.WriteLine("\n");
+
+            Console.WriteLine("SOLID principles used in this pattern");
+
+            Console.WriteLine("- ");
+            Console.WriteLine("- ");
+            Console.WriteLine("- ");
+            Console.WriteLine("- ");
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Problem");
+            TestDesignPatternProblem();
+
+            Console.WriteLine("solution Code");
+            TestDesignPatternSolution();
+        }
+
+        public void TestDesignPatternProblem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TestDesignPatternSolution()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
