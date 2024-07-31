496c795cc4e2b4875937b6b0892c91526c6bbf91
Ftoon Bin Rbia <fbinrbia@bcare.com.sa>
2024-07-31 11:27:31 +0300

    add mediator design pattern folders

---
+++ DesignPatterns/MediatorPattern/TestMediatorPattern.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.MediatorPattern
+{
+    internal class TestMediatorPattern : ITestState, ITestProblemState, ITestSolutionState
+    {
+        public void Test()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TestDesignPatternProblem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TestDesignPatternSolution()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
