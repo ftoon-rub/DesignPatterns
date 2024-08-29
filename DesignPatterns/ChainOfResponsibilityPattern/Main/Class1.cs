dba02e466356ec56dfcdd2bab4b7caf29f2c9ca6
Ftoon Bin Rbia <fbinrbia@bcare.com.sa>
2024-08-29 08:04:34 +0300

    add the structuer for chain of responsoibility patten

--- DesignPatterns/ChainOfResponsibilityPattern/Main/Class1.cs
+++ DesignPatterns/ChainOfResponsibilityPattern/Main/Class1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DesignPatterns.ChainOfResponsibilityPattern
+namespace DesignPatterns.ChainOfResponsibilityPattern.Main
 {
     internal class Class1
     {
---
+++ DesignPatterns/ChainOfResponsibilityPattern/TestChainOfResponsibilityPattern.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.ChainOfResponsibilityPattern
+{
+    public class TestChainOfResponsibilityPattern : ITestState, ITestMainState, ITestProblemState, ITestSolutionState
+    {
+        public void Test()
+        {
+            TestDesignPatternMain();
+            TestDesignPatternProblem();
+            TestDesignPatternSolution();
+        }
+
+        public void TestDesignPatternMain()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TestDesignPatternProblem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TestDesignPatternSolution()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
