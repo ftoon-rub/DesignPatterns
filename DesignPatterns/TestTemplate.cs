de51efb7cd292eb1bc9746f171aa87950f347060
Ftoon Bin Rbia <fbinrbia@bcare.com.sa>
2024-08-29 08:49:47 +0300

    test template

---
+++ DesignPatterns/TestTemplate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public abstract class TestTemplate<T> 
+    {
+        public void DescribePattern()
+        {
+            PatternCategory();
+            PatternDescription();
+            PatternComponents();
+            SolidUsedInPattern();
+            OopUsedInPattern();
+            TestDesignPatternMain();
+            TestDesignPatternProblem();
+            TestDesignPatternSolution();
+        }
+        protected abstract void PatternCategory();
+        protected abstract void PatternDescription();
+        protected abstract void PatternComponents();
+        protected abstract void SolidUsedInPattern();
+        protected abstract void OopUsedInPattern();
+        protected abstract void TestDesignPatternMain();
+        protected abstract void TestDesignPatternProblem();
+        protected abstract void TestDesignPatternSolution();
+    }
+}
