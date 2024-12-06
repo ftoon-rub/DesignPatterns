190eb33185085113c74deb289c2485a24a5d7bd9
Ftoon Alrubayea <ftoon-rubayea@hotmail.com>
2024-12-06 11:02:53 +0300

    create foldre for prototypepattern

---
+++ DesignPatterns/CreationalDesignPatterns/PrototypePattern/TestPrototypePattern.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.CreationalDesignPatterns.PrototypePattern
+{
+    public class TestPrototypePattern : TestCreationalDesignPatternsTemplate
+    {
+        protected override void OopUsedInPattern()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void PatternComponents()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void PatternDescription()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void SolidUsedInPattern()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void TestDesignPatternMain()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
