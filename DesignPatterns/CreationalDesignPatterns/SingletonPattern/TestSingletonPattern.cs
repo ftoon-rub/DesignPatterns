5694645a2a454679d3752e102a2bfcc8a928c19b
Ftoon Alrubayea <ftoon-rubayea@hotmail.com>
2024-12-13 19:37:27 +0300

    patch 1 singleton patten fils structer

---
+++ DesignPatterns/CreationalDesignPatterns/SingletonPattern/TestSingletonPattern.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.CreationalDesignPatterns.SingletonPattern
+{
+    public class TestSingletonPattern : TestCreationalDesignPatternsTemplate
+    {
+        protected override void Ex1()
+        {
+            Console.WriteLine("\n");
+        }
+
+        protected override void OopUsedInPattern()
+        {
+            Console.WriteLine("\n");
+        }
+
+        protected override void PatternComponents()
+        {
+            Console.WriteLine("\n");
+        }
+
+        protected override void PatternDescription()
+        {
+            Console.WriteLine("\n");
+        }
+
+        protected override void SolidUsedInPattern()
+        {
+            Console.WriteLine("\n");
+        }
+
+        protected override void TestDesignPatternMain()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
